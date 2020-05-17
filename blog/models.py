a55f46217ef72cb17afd06ed722ea4b3c1a001ec
Your Name <you@example.com>
2020-05-17 16:17:24 +0900

    Modified templates to display posts from database.

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,7 @@ from django.utils import timezone
 
 
 class Post(models.Model):
+    id = models.AutoField(primary_key=True)
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
